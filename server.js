6fd7e84814d9e801fee8c6fd0a270d2b4cb27583
William MacDonald <wmsmacdonald@gmail.com>
2016-07-19 22:29:05 -0500

    text history

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const https = require('https');
 const diff_match_patch = require('./diff_match_patch');
 const diff = new diff_match_patch.diff_match_patch();
 const pug = require('pug');
+const uuid = require('node-uuid');
+const TextHistory = require('./text_history');
 
 //const testFile = fs.readFileSync('./test_files/test1.txt');
 
-let versionHistory = {};
+let resourceHistories = {};
 
 let options = {
   key: fs.readFileSync('/home/bill/.ssh/key.pem'),
@@ -22,28 +24,29 @@ app.use(express.static('test/public'));
 
 app.get('/dynamic.html', function (req, res) {
   let date = new Date().toString();
-  let response = '<h1>' + date + '</h1>';
+  let responseBody = '<h1>' + date + '</h1>';
 
-  // no version has been created yet
-  if (versionHistory[req.route.path] === undefined) {
-    versionHistory[req.route.path] = {};
+  // if there isn't a resource history yet
+  if (resourceHistories[req.route.path] === undefined) {
+    resourceHistories[req.route.path] = TextHistory();
   }
-  versionHistory[req.route.path][date] = response;
-  res.header('Delta-Version', date);
+
+  let id = resourceHistories[req.route.path].addVersion(responseBody);
+
+
+  res.header('Delta-Version', id);
   if (req.headers['delta-version'] === undefined) {
-    res.header('Content-Type', 'text/html');
-    return res.end(response);
+
+    return res.end(resourceHistories[req.route.path].lastVersion);
   }
   // client has a cached version of the page
   else {
-    let clientVersion = versionHistory[req.route.path][req.headers['delta-version']];
-    // version not known
-    if (clientVersion === undefined) {
-      res.header('Content-Type', 'text/html');
-      return res.end(response);
+    let patches = resourceHistories[req.route.path].getPatches(req.headers['delta-version']);
+
+    if (patches === undefined) {
+      return res.end(responseBody);
     }
     else {
-      let patches = diff.patch_make(clientVersion, response);
       res.header('Delta-Patch', 'true');
       console.log(JSON.stringify(patches));
       return res.end(JSON.stringify(patches));
@@ -52,4 +55,3 @@ app.get('/dynamic.html', function (req, res) {
 });
 
 https.createServer(options, app).listen(8000);
-
